26e5acc94ffbdbbc888defb0927590175493d9d6
Steve0a0 <stephenangelo4@gmail.com>
2024-09-16 00:23:41 +0100

    OBU01UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import { Home } from '@mui/icons-material';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+function App() {
+  return (<>
+    <Navbar company= "OneBill Utilities"/>
+    <Home />
+ </> );
+}
+
+export default App;
